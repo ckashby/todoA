03e0ab8c8e71c608dfa582c4c7c3b3e7ff765aad
C K Ashby <clay.ashby@gmail.com>
2015-09-14 18:39:59 -0700

    initial

---
+++ app/src/main/java/com/meteoru/kalei/todo2/MainActivity.java
@@ -0,0 +1,62 @@
+package com.meteoru.kalei.todo2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lvItems;
+    ArrayList<String> alItems;
+    ArrayAdapter<String> aaItems;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        alItems = new ArrayList<String>();
+        aaItems = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alItems
+        );
+        lvItems.setAdapter(aaItems);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onAddItem(View view) {
+        EditText etAddItem = (EditText) findViewById(R.id.etAddItem);
+        aaItems.add(etAddItem.getText().toString());
+        etAddItem.setText("");
+
+    }
+}
